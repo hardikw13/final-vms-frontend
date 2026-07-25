568f39fcb4e296ebc3fdaa5abef8adb6b0eb24bb
gyanesh027 <gyanesh20061@gmail.com>
2026-07-25 14:18:03 +0530

    link

--- VMS FRONTEND/accept-invite.js
+++ VMS FRONTEND/accept-invite.js
@@ -1,4 +1,4 @@
-const API_URL = "https://edugate-9yl5.onrender.com";
+const API_URL = "https://edugate-9yl5.onrender.com/api";
 
 const token = new URLSearchParams(window.location.search).get("token");
 
